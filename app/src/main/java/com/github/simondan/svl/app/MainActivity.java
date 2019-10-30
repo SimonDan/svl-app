fb257db38124a0c8ed817877b134f00b36330c8a
Johanna <s-jbaume@haw-landshut.de>
2019-10-30 20:58:57 +0100

    Login Maske

--- app/src/main/java/com/github/simondan/svl/app/MainActivity.java
+++ app/src/main/java/com/github/simondan/svl/app/MainActivity.java
@@ -1,40 +1,67 @@
 package com.github.simondan.svl.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = "xx Main Activity";
+
+   //UI Variablen Deklarieren
+    EditText mEditTextVorName;
+    EditText mEditTextNachName;
+    EditText mEditTextMail;
+    Button mButtonCreateAccount;
+    Button mButtonCreateAccount2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+
+        //UI Variablen initialisieren
+        mEditTextVorName = (EditText) findViewById(R.id.main_create_Account_EditText_VorName);
+        mEditTextNachName = (EditText) findViewById(R.id.main_create_Account_EditText_NachName);
+        mEditTextMail = (EditText) findViewById(R.id.main_create_Account_EditText_Mail);
+        mButtonCreateAccount = (Button) findViewById(R.id.main_create_Account_Button);
+        mButtonCreateAccount2 = (Button) findViewById(R.id.main_create_Account_Button2);
+
+        //Listener registrieren
+        mButtonCreateAccount.setOnClickListener(this);
+        mButtonCreateAccount2.setOnClickListener(this);
+
+
+
+
+        //FloatingActionButton fab = findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+           // public void onClick(View view) {
+            //    Snackbar.make(view, "Hier geht es zu den Strafen", Snackbar.LENGTH_LONG)
+            //            .setAction("Action", null).show();
+          //  }
+       // });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflator = getMenuInflater();
+        inflator.inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -52,4 +79,64 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart called");
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        switch (i) {
+            case R.id.main_create_Account_Button:
+                Toast.makeText(getApplicationContext(),
+                        "Create Account pressed", Toast.LENGTH_LONG).show();
+                return;
+            case R.id.main_create_Account_Button2:
+               Intent intent = new Intent (this,RepeatedLogin.class);
+               startActivity(intent);
+               this.finish();
+                return;
+        }
+    }
+
+
+
+
 }
---
+++ app/src/main/java/com/github/simondan/svl/app/RepeatedLogin.java
@@ -0,0 +1,62 @@
+package com.github.simondan.svl.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RepeatedLogin extends AppCompatActivity implements View.OnClickListener {
+
+    Button mButton;
+    Button mButton2;
+    Button mButton3;
+    EditText mEdittext;
+    EditText mEdittext2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repeated_login);
+
+        mEdittext = (EditText) findViewById(R.id.activity_repeated_login_EditText_Mail);
+        mEdittext2 = (EditText) findViewById(R.id.activity_repeated_login_Passwort);
+        mButton = (Button) findViewById(R.id.activity_repeated_login_Button_Senden);
+        mButton2 = (Button) findViewById(R.id.activity_repeated_login_Button_Senden_Server);
+        mButton3 = (Button) findViewById(R.id.activity_repeated_login_Button_Zurück);
+
+        mButton.setOnClickListener(this);
+        mButton2.setOnClickListener(this);
+        mButton3.setOnClickListener(this);
+        mEdittext.setOnClickListener(this);
+        mEdittext2.setOnClickListener(this);
+
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        switch (i) {
+            case R.id.activity_repeated_login_Button_Senden:
+                Toast.makeText(getApplicationContext(),
+                        "Sie haben soeben ein Passwort an diese Mailadresse erhalten", Toast.LENGTH_LONG).show();
+                return;
+            case R.id.activity_repeated_login_Button_Senden_Server:
+                Toast.makeText(getApplicationContext(),
+                        "Das Passwort wurde an den Server erfolgreich gesendet. Starten Sie die App neu.", Toast.LENGTH_LONG).show();
+                return;
+            case R.id.activity_repeated_login_Button_Zurück:
+                Intent intent = new Intent (this,MainActivity.class);
+                startActivity(intent);
+                this.finish();
+                return;
+
+        }
+    }
+}
